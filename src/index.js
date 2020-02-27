7c042331434e44dc1c8569eb6be3d32211d3c21d
Suzy <suzy.nakayama@gmail.com>
2020-02-27 18:32:46 -0500

    basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./pages/App/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <Router>
+    <Router basename="/">
         <Route component={App} />
     </Router>,
     document.getElementById("root")
