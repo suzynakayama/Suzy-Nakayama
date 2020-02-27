45111fbc61a803b88733ecdf2a4cd7ad7e081ffd
Suzy <suzy.nakayama@gmail.com>
2020-02-27 18:55:12 -0500

    back to browser router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
 import App from "./pages/App/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <Router basename="/">
+    <Router>
         <Route component={App} />
     </Router>,
     document.getElementById("root")
