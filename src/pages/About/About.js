08a7a1da27290ee6138a8fb581a038dbce31a8c7
Suzy <suzy.nakayama@gmail.com>
2020-04-02 16:42:06 -0400

    Updated About and the resume

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,38 +3,36 @@ import SpecialtyCard from "../../components/SpecialtyCard/SpecialtyCard";
 import "./About.css";
 
 function About() {
-    return (
-        <div className="about" id="about">
-            <h2 className="about__title">About Me</h2>
-            <div className="about__container">
-                <div className="about__text">
-                    <p>
-                        Hi, I'm Suzy Nakayama and I graduated from General
-                        Assembly <strong>Software Engineering Immersive</strong>{" "}
-                        program. I am a creative full-stack developer that
-                        translates the geekiness behind ideas bringing to life
-                        an aesthetically pleasing and fluid app for users.
-                    </p>
-                    <p>
-                        My background in intellectual property law and culinary
-                        improved my ability to articulate in a business context
-                        and to provide creative solutions. This allows me to be
-                        an organized and focused team player that collaborate
-                        for a positive environment seeking for challenges and
-                        opportunities to grow and improve, while also developing
-                        and delivering.
-                    </p>
-                    <h3 className="about__text--phrase">
-                        Learning is my hobby and creating is my passion.
-                    </h3>
-                </div>
-                <div className="about__line"></div>
-                <div className="about__specialties">
-                    <SpecialtyCard />
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className='about' id='about'>
+			<h2 className='about__title'>About Me</h2>
+			<div className='about__container'>
+				<div className='about__text'>
+					<p>
+						Hi, I'm Suzy Nakayama and I graduated from General Assembly{" "}
+						<strong>Software Engineering Immersive</strong> program. I am a
+						full-stack developer that translates the geekiness behind ideas
+						bringing to life an aesthetically pleasing and fluid app for users.
+					</p>
+					<p>
+						My background in intellectual property law and culinary arts
+						enhanced my ability to articulate in a business context and to
+						provide creative solutions. This allows me to be an organized and
+						focused team player that collaborates for a positive environment
+						seeking challenges and opportunities to grow and improve, while also
+						developing and delivering.
+					</p>
+					<h3 className='about__text--phrase'>
+						Learning is my hobby and creating is my passion.
+					</h3>
+				</div>
+				<div className='about__line'></div>
+				<div className='about__specialties'>
+					<SpecialtyCard />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default About;
