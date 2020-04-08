7635f30aec6269c81afd2e39b1112503beef4069
Suzy <suzy.nakayama@gmail.com>
2020-04-08 10:27:20 -0400

    added new skills, changed about line, and fixed box shadow

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,54 +2,54 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-    return (
-        <div className="footer">
-            <p>
-                <span className="footer__span">Developed by</span>
-                &nbsp;Suzy Nakayama&nbsp;
-            </p>
-            <ul className="social-list">
-                <li className="social-list__item">
-                    <a
-                        className="social-list__link"
-                        href="files/suzynakayama.docx"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        <i class="far fa-file-word"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a
-                        className="social-list__link"
-                        href="https://www.instagram.com/suzyyume/"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        <i className="fab fa-instagram"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a
-                        className="social-list__link"
-                        href="https://www.linkedin.com/in/suzy-nakayama/"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a
-                        className="social-list__link"
-                        href="https://github.com/suzynakayama"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        <i className="fab fa-github"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
+	return (
+		<div className="footer">
+			<p>
+				<span className="footer__span">Developed by</span>
+				&nbsp;Suzy Nakayama&nbsp;
+			</p>
+			<ul className="social-list">
+				<li className="social-list__item">
+					<a
+						className="social-list__link"
+						href="files/suzynakayama.docx"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<i className="far fa-file-word"></i>
+					</a>
+				</li>
+				<li className="social-list__item">
+					<a
+						className="social-list__link"
+						href="https://www.instagram.com/suzyyume/"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<i className="fab fa-instagram"></i>
+					</a>
+				</li>
+				<li className="social-list__item">
+					<a
+						className="social-list__link"
+						href="https://www.linkedin.com/in/suzy-nakayama/"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<i className="fab fa-linkedin"></i>
+					</a>
+				</li>
+				<li className="social-list__item">
+					<a
+						className="social-list__link"
+						href="https://github.com/suzynakayama"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<i className="fab fa-github"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
